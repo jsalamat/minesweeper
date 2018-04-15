3ef3bccb8da2cb63be549127c6b0252eaccd923f
jsalamat <salamat_jon@yahoo.com>
2018-04-14 21:28:04 -0400

    combining pipeline elements in an array string using join method

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,16 @@
 const printBoard = board => {
-
+  console.log('Current Board: ')
+  // array.join combine elements in an array string 
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
 };
 
-let board = [
+const board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
+//console.log(board);
 
-console.log(board);
+printBoard(board);
