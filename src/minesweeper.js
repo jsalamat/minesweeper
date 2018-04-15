645de10af8c72a15bc8dfe7d22ee8a444db4c38e
jsalamat <salamat_jon@yahoo.com>
2018-04-14 21:47:40 -0400

    editing so parameter is readable

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,10 @@
+const exampleBoard = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+//console.log(exampleBoard);
+
 const printBoard = board => {
   console.log('Current Board: ')
   // array.join combine elements in an array string 
@@ -6,18 +13,10 @@ const printBoard = board => {
   console.log(board[2].join(' | '));
 };
 
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
-//console.log(board);
-
-printBoard(board);
-
-board[0][1]='1';
-board[2][2]='B';
-printBoard(board);
+printBoard(exampleBoard);
+exampleBoard[0][1]='1';
+exampleBoard[2][2]='B';
+printBoard(exampleBoard);
 
 // You created a printBoard() function. How might it be improved so that each nested array doesn't have to be manually joined?
 //Once again, the game board was hard coded (board). How might you dynamically generate this in the future?
